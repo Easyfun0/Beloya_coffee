6dd8c9b1424ac71c70e0e890c15b14c815bc54c7
Easyfun0 <s5300012@hotmail.com>
2019-08-09 03:10:08 +0800

    third

--- js/all.js
+++ js/all.js
@@ -1,27 +1,46 @@
-const xhr = new XMLHttpRequest();
+// const xhr = new XMLHttpRequest();
 
-xhr.open('get',
-    'https://data.taipei/api/getDatasetInfo/downloadResource?id=8a569b5b-0cb1-4c7f-a064-780b3301354c&rid=639b4d6f-e58f-47b1-900d-c2a692c436cf',
-    true
-);
+// xhr.open('get',
+//     'https://data.taipei/api/getDatasetInfo/downloadResource?id=8a569b5b-0cb1-4c7f-a064-780b3301354c&rid=639b4d6f-e58f-47b1-900d-c2a692c436cf',
+//     true
+// );
 
-xhr.send(null);
+// xhr.send(null);
 
-xhr.onload = function () {
+// xhr.onload = function () {
 
-    if (xhr.status == 200) {
-        let str = JSON.parse(xhr.responseText);
-        document.querySelector('.name').textContent = str[15].品牌名稱
-        document.querySelector('.message').textContent = str[15].品牌故事
-        document.querySelector('.name1').textContent = str[2].品牌名稱
-        document.querySelector('.message1').textContent = str[2].品牌故事
-        document.querySelector('.name2').textContent = str[3].品牌名稱
-        document.querySelector('.message2').textContent = str[3].品牌故事
-        document.querySelector('.name3').textContent = str[4].品牌名稱
-        document.querySelector('.message3').textContent = str[4].品牌故事
+//     if (xhr.status == 200) {
+//         let str = JSON.parse(xhr.responseText);
+//         document.querySelector('.name').textContent = str[15].品牌名稱
+//         document.querySelector('.message').textContent = str[15].品牌故事
+//         document.querySelector('.name1').textContent = str[2].品牌名稱
+//         document.querySelector('.message1').textContent = str[2].品牌故事
+//         document.querySelector('.name2').textContent = str[3].品牌名稱
+//         document.querySelector('.message2').textContent = str[3].品牌故事
+//         document.querySelector('.name3').textContent = str[4].品牌名稱
+//         document.querySelector('.message3').textContent = str[4].品牌故事
 
 
-    } else {
-        console.log('錯誤');
-    }
+//     } else {
+//         console.log('錯誤');
+//     }
+// }
+
+
+let el = document.querySelector('.move');
+
+el.onmouseover = function () {
+    console.log('ggg')
+
+}
+
+
+let a = ['a', 'b', 'c']
+
+for (let i of a.keys()) {
+    console.log(i);
+}
+
+for (let i of a.entries()) {
+    console.log(i);
 }
